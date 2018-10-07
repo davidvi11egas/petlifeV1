40ea100bc05e34a02487054c606d369c37e04f45
Melissa A <41melissa.allen@gmail.com>
2018-10-06 23:47:26 -0400

    update ignore files

---
+++ petlife/build/precache-manifest.84558c5899896baf97d4f66c3c14c860.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fe3d40b3b8e70bfe2971",
+    "url": "/static/js/main.fe3d40b3.chunk.js"
+  },
+  {
+    "revision": "0cd983c2aeefbcd5dd91",
+    "url": "/static/js/1.0cd983c2.chunk.js"
+  },
+  {
+    "revision": "fe3d40b3b8e70bfe2971",
+    "url": "/static/css/main.26b349c3.chunk.css"
+  },
+  {
+    "revision": "9f6cb10a7fe58d9e3d672ebd2eaad024",
+    "url": "/index.html"
+  }
+];
